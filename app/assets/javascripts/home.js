183f357e90b42a3e53cf567755bc46c07708532c
Matthew Costabile <mattcosta7@gmail.com>
2016-05-30 12:26:49 -0400

    face-icon

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,5 +5,5 @@ $(document).ready(function(){
     $('#message').val('');
     $('form').prepend("<p class='red-text text-darken-4'>Message Successfully Sent, I'll get back to you shortly</p>");
   });
-
+  
 });
